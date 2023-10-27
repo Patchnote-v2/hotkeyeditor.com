8cf7c658d66c8654e2bfa0ebc4f3945eeb327521
Patchnote-v2 <11915225+Patchnote-v2@users.noreply.github.com>
2023-10-26 18:55:29 -0700

    Added view to send files to

--- src/hotkeys/views.py
+++ src/hotkeys/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import parsers, response, views
 
-# Create your views here.
+
+class UploadHKPView(views.APIView):
+    parser_classes = [parsers.FileUploadParser]
+
+    def put(self, request, filename, format=None):
+        file_obj = request.data['file']
+        # ...
+        # do some stuff with uploaded file
+        # ...
+        return response.Response({'test': True, }, status=204)
