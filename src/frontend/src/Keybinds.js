1f5a3b911f6b15df25980ac0d51eb04679582291
Patchnote-v2 <11915225+Patchnote-v2@users.noreply.github.com>
2023-11-06 19:42:47 -0800

    Updated Keybinds to use useEffect and have keybinds be a state variable

--- src/frontend/src/Keybinds.js
+++ src/frontend/src/Keybinds.js
@@ -1,49 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { keyNames } from "./keyNames.js";
 
 const Keybinds = (data) => {
-    var keybinds;
-    if (Object.keys(data.data).length > 0) {
-        keybinds = Object.keys(data.data.groups).map((value, key) => {
-            return (
-                <>
-                <h3>{value}</h3>
-                {data.data.groups[value].map((value2, key2) => {
-                    var hotkey = data.data.hotkeys[data.data.groups[value][key2]];
-                    // console.log(hotkey);
-                    if (hotkey) {
-                        let rowClassNames = "keycode";
-                        rowClassNames += data.buffer && value2 == data.buffer.id ? " active-keybind" : "";
-                        return (
-                            <li key={value2}
-                                id={value2}
-                                data-keycode={hotkey.keycode}
-                                data-ctrl={hotkey.ctrl}
-                                data-shift={hotkey.shift}
-                                data-alt={hotkey.alt}
-                                onMouseOver={data.updateCurrentHoverCallback}
-                                onMouseOut={data.updateCurrentHoverCallback}
-                                onClick={(event) => data.handleSettingKeybind(event)}
-                                className={rowClassNames}>
-                            <span className="keycode-text">{hotkey.string_text} -- </span>
-                            <span className="keycode-code">
-                                {hotkey.ctrl ? "Ctrl+" : ""}
-                                {hotkey.shift ? "Shift+" : ""}
-                                {hotkey.alt ? "Alt+" : ""}
-                                {keyNames[hotkey.keycode] ? keyNames[hotkey.keycode] : String.fromCharCode(hotkey.keycode).toUpperCase()}
-                            </span>
-                            </li>
-                        );
-                    }
-                    else {
-                        return;
-                    }
-                })}
-                </>
-            );
-        });
-    }
+    const [keybinds, setKeybinds] = useState();
+    
+    useEffect(() => {
+        if (data.data) {
+            let temp = Object.keys(data.data.groups).map((value, key) => {
+                return (
+                    <>
+                    <h3>{value}</h3>
+                    {data.data.groups[value].map((value2, key2) => {
+                        var hotkey = data.data.hotkeys[data.data.groups[value][key2]];
+                        if (hotkey) {
+                            let rowClassNames = "keycode";
+                            rowClassNames += data.buffer && value2 == data.buffer.id ? " active-keybind" : "";
+                            if (value2 == 19327) {
+                                console.log("HOTKEYCONTROL");
+                                console.log(hotkey);
+                            }
+                            return (
+                                <li key={value2}
+                                    id={value2}
+                                    data-keycode={hotkey.keycode}
+                                    data-ctrl={hotkey.ctrl}
+                                    data-shift={hotkey.shift}
+                                    data-alt={hotkey.alt}
+                                    onMouseOver={data.updateCurrentHoverCallback}
+                                    onMouseOut={data.updateCurrentHoverCallback}
+                                    onClick={(event) => data.handleSettingKeybind(event)}
+                                    className={rowClassNames}>
+                                <span className="keycode-text">{hotkey.string_text} -- </span>
+                                <span className="keycode-code">
+                                    {hotkey.ctrl ? "Ctrl+" : ""}
+                                    {hotkey.shift ? "Shift+" : ""}
+                                    {hotkey.alt ? "Alt+" : ""}
+                                    {keyNames[hotkey.keycode] ? keyNames[hotkey.keycode] : String.fromCharCode(hotkey.keycode).toUpperCase()}
+                                </span>
+                                </li>
+                            );
+                        }
+                        else {
+                            return;
+                        }
+                    })}
+                    </>
+                );
+            })
+            setKeybinds(temp);
+        }
+    }, [data])
     
     return (
         <>
