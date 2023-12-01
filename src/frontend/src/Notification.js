c86168aa14313078f18b29b67e670506ba7277fc
Patchnote-v2 <11915225+Patchnote-v2@users.noreply.github.com>
2023-12-01 13:09:42 -0800

    Added removeNotification function to remove a notification when click on its X

--- src/frontend/src/Notification.js
+++ src/frontend/src/Notification.js
@@ -5,6 +5,12 @@ import Utils from "./Utils.js";
 const Notifications = forwardRef((props, ref) => {
     const [notifications, setNotifications] = useState([]);
     
+    const removeNotification = (index) => {
+        let oldNotifications = [...notifications];
+        oldNotifications.splice(index, 1);
+        setNotifications(oldNotifications);
+    }
+    
     const addNotification = (message) => {
         setNotifications(oldNotifications => [message, ...oldNotifications]);
     }
@@ -15,19 +21,17 @@ const Notifications = forwardRef((props, ref) => {
         };
     });
     
-    const listNotifications = notifications.map((each) => {
-        return (
-            <div className="notification">
-                <span className="messaeg" dangerouslySetInnerHTML={{ __html: each }} />
-                <span className="close-notification">✕</span>
-            </div>
-        );
-    })
-    
     return (
         <div ref={ref}
              id="notifications-wrapper">
-                {listNotifications}
+            {notifications.map((each, index) => {
+                return (
+                    <div className="notification">
+                        <span className="message" dangerouslySetInnerHTML={{ __html: each }} />
+                        <span className="close-notification" onClick={e => removeNotification(index)}>✕</span>
+                    </div>
+                );
+            })}
         </div>
     );
 });
