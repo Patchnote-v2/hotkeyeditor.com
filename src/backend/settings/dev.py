b0025d82571beaac92417a961a871ec6f5551bc5
Patchnote-v2 <11915225+Patchnote-v2@users.noreply.github.com>
2023-12-07 20:22:28 -0800

    Fixed DEBUG not getting set as True

--- src/backend/settings/dev.py
+++ src/backend/settings/dev.py
@@ -3,11 +3,13 @@ import environ
 
 logging.debug("Settings loading: %s" % __file__)
 
+from .base import *  # noqa
+
 # This will read missing environment variables from a file
 # We wan to do this before loading a base settings as they may depend on environment
 environ.Env.read_env(DEBUG='True')
-
-from .base import *  # noqa
+env = environ.Env()
+DEBUG = env("DEBUG", default=True)
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
